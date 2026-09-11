8eb207ffb9276096fd69a5712dd6ce2e7acd56dd
HenriqueBossle <henriquegoncalvesbossle@gmail.com>
2026-09-11 18:39:16 -0300

    insert chat stmt

--- php/insert-chat.php
+++ php/insert-chat.php
@@ -2,17 +2,45 @@
 
 session_start();
 
-if(isset($_SESSION['unique_id'])){
+if (isset($_SESSION['unique_id'])) {
+
     require_once __DIR__ . "/config.php";
 
     $outgoing_id = $_SESSION['unique_id'];
-    $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
 
-    $message = mysqli_real_escape_string($conn, $_POST['message']);
+    $incoming_id = filter_input(
+        INPUT_POST,
+        'incoming_id',
+        FILTER_VALIDATE_INT
+    );
 
-    if(!empty($message)){
-        $sql = mysqli_query($conn, "INSERT INTO messages(incoming_msg_id, outgoing_msg_id, msg) VALUES ({$incoming_id}, {$outgoing_id}, '{$message}')") or die();
+    $message = trim($_POST['message'] ?? '');
+
+    if (!$incoming_id || $message === '') {
+        exit;
     }
-}else{
-    header("location: ../login.php");
+
+    $stmt = mysqli_prepare(
+        $conn,
+        'INSERT INTO messages
+        (incoming_msg_id, outgoing_msg_id, msg)
+        VALUES (?, ?, ?)'
+    );
+
+    mysqli_stmt_bind_param(
+        $stmt,
+        'iis',
+        $incoming_id,
+        $outgoing_id,
+        $message
+    );
+
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_close($stmt);
+
+} else {
+
+    header("Location: ../login.php");
+    exit;
 }

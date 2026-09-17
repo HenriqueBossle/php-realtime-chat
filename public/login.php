356d1c932abc8e3a85738aec56b52dddcf53bb67
HenriqueBossle <henriquegoncalvesbossle@gmail.com>
2026-09-17 15:08:19 -0300

    front-end

--- public/login.php
+++ public/login.php
@@ -12,28 +12,26 @@
 <body>
     <div class="wrapper">
         <section class="form login">
-            <header>Realtime Chat App</header>
+            <header>Bem-vindo <span>de volta</span></header>
             <form action="#" method="post" autocomplete="off">
                 <div class="error text"></div>
                 <div class="field input">
-                    <label>Email Address</label>
-                    <input type="email" name="email" placeholder="Enter your email" required>
+                    <label>E-mail</label>
+                    <input type="email" name="email" placeholder="seuemail@exemplo.com" required>
                 </div>
 
                 <div class="field input">
-                    <label>Password</label>
-                    <input type="password" name="password" placeholder="Enter your password" required>
+                    <label>Senha</label>
+                    <input type="password" name="password" placeholder="Sua senha" required>
                     <i class="fas fa-eye"></i>
                 </div>
 
-                
-
                 <div class="field button">
-                    <input type="submit" name="submit" value="continue to chat">
+                    <input type="submit" name="submit" value="Entrar no chat">
                 </div>
             </form>
 
-            <div class="link" >Not yet signed up?<a href="index.php">Signup now</a></div>
+            <div class="link">Ainda não tem conta? <a href="index.php">Cadastrar agora</a></div>
         </section>
     </div>
 
